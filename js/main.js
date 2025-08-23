fcf58a3a74d4e599e5655a9af1cfad2ed66f2119
MaWay2000 <mantasmatuseviciuz@gmail.com>
2025-08-23 15:40:08 +0000

    some fixes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 import('./game.js');
+// Choose the correct base path for loading the ES module depending on
+// where the page is hosted. htmlpreview.github.io serves files from
+// raw.githubusercontent.com which delivers JavaScript as text/plain,
+// causing module loading to fail. To work around this, load the module
+// from the jsDelivr CDN when running under htmlpreview; otherwise use
+// a relative path for local usage.
+const baseUrl = (location.hostname === 'htmlpreview.github.io')
+  ? 'https://cdn.jsdelivr.net/gh/MaWay2000/wzmap/'
+  : './';
+
+import(`${baseUrl}js/game.js`);
