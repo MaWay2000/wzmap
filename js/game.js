1fc0bdef5c28d81e84f2dc29acd48a71448dba15
MaWay2000 <mantasmatuseviciuz@gmail.com>
2025-08-26 01:26:43 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ import * as THREE from './three.module.js';
 // https://github.com/MaWay2000/wzmap/edit/main/js/game.js#L3C7
 // https://github.dev/MaWay2000/wzmap/blob/main/js/game.js cia testinu
 
-
+// aaa main
 
 /// cia editinu: https://github.com/MaWay2000/wzmap/edit/main/js/game.js
 /// tada paleidziu purge https://purge.jsdelivr.net/gh/MaWay2000/wzmap@main/js/game.js
