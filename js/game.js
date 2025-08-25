ce4fc70fad463fe25571356c009c602ac6f6e81b
MaWay2000 <mantasmatuseviciuz@gmail.com>
2025-08-25 08:30:23 +0000

    tile count

--- js/game.js
+++ js/game.js
@@ -4,9 +4,15 @@ import { TILESETS, loadAllTiles } from './tileset.js';
 const tilesetSelect = document.getElementById('tilesetSelect');
 const texturePalette = document.getElementById('texturePalette');
 
+let currentPaletteRequest = 0;
+
 async function refreshPalette(idx) {
+  const requestId = ++currentPaletteRequest;
   texturePalette.innerHTML = "";
   const images = await loadAllTiles(idx);
+  // If another tileset change happened while we were loading,
+  // abort updating the palette to avoid mismatched tiles.
+  if (requestId !== currentPaletteRequest) return;
   images.forEach((img, i) => {
     const div = document.createElement("div");
     div.style.width = "32px";
